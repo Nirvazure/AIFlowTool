1c9971fec37b79646b7ecd8dd4099a99c269fbcf
李富荣 <lifurong1@cloudwalk.com>
2020-09-23 16:35:48 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,22 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'API_Tester',
     component: () => import('../views/API_Tester.vue')
   },
   {
-    path:'/chat',
-    name:'Chat',
-    component:()=>import('@/views/Chat')
+    path: '/chat',
+    name: 'Chat',
+    component: () => import('@/views/Chat')
   },
-  {
-    path:'/chat2',
-    name:'Chat2',
-    component:()=>import('@/views/Chat2')
-  }
 ]
 
 const router = new VueRouter({
   // mode: 'history',
   // base: process.env.BASE_URL,
-  base:'/',
+  base: '/',
   routes
 })
 
